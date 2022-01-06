b9989e8bfc07872eb91aca07aa8323d5a0cc374a
Albi Thomson K <albithomson@gmail.com>
2022-01-06 08:28:14 +0530

    Updates

---
+++ precache-manifest.82eeb2e504a8059a9ba01ceb14ad140f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a316c84238e1b60428010a43dacf5b9a",
+    "url": "/Messenger-clone/index.html"
+  },
+  {
+    "revision": "ae15fddaf14040b0b45a",
+    "url": "/Messenger-clone/static/css/main.44b6f42f.chunk.css"
+  },
+  {
+    "revision": "b761637dfa1a909e3731",
+    "url": "/Messenger-clone/static/js/2.6e89c4fc.chunk.js"
+  },
+  {
+    "revision": "9cf9d893b5c51de0aa6dd9e07be64537",
+    "url": "/Messenger-clone/static/js/2.6e89c4fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae15fddaf14040b0b45a",
+    "url": "/Messenger-clone/static/js/main.04db8bc5.chunk.js"
+  },
+  {
+    "revision": "8fdaaaa64cb500f4bee7",
+    "url": "/Messenger-clone/static/js/runtime-main.67602ee3.js"
+  }
+]);
